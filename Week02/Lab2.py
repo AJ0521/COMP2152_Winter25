c08937708c00048df7cf24dc27f6b8cfe6d5faae
Abrar Junaid <abrarjunaid.mohammed@georgebrown.ca>
2025-01-14 10:34:22 -0500

    add elements array

---
+++ Week02/Lab2.py
@@ -0,0 +1,14 @@
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
+#print("Elements: ", elements)
+# git add . && git commit -m "add elements array" && git push
+
+#def funct_name():
+#   return True
+
+def say_greeting(name, message):
+    print(f" {message}, {name}")
+
+    say_greeting("AJ")
+    say_greeting("AJ", "Hello")
+
+
